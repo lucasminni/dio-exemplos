ba40e0958f64beffed3e28aba6987795ba006186
Minni <lucasminni@hotmail.com>
2022-12-24 19:04:46 -0300

    construtores

---
+++ exemplos/src/SistemaCadastro.java
@@ -0,0 +1,9 @@
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("Marcos","123456");
+
+        marcos.setEndereco("Rua das Marias");
+
+        System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+    }
+}
